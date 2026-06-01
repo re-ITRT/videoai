c5a3f1f3b36895fe4bdebd52c8260934fff81aad
毛治钦 <1940428933@qq.com>
2026-06-01 18:59:44 +0800

    fix: 删studio路由

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import Register from './pages/register'
 import Dashboard from './pages/dashboard'
 import Reference from './pages/reference'
 import Templates from './pages/templates'
-import Studio from './pages/studio'
 import MaterialList from './modules/material/index'
 import MaterialUpload from './modules/material/upload'
 import ScriptList from './modules/script/index'
@@ -27,7 +26,6 @@ function App() {
           <Route index element={<Dashboard />} />
           <Route path="reference" element={<Reference />} />
           <Route path="templates" element={<Templates />} />
-          <Route path="studio" element={<Studio />} />
           <Route path="material" element={<MaterialList />} />
           <Route path="material/upload" element={<MaterialUpload />} />
           <Route path="script" element={<ScriptList />} />
